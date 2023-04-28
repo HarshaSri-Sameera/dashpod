278c0f1798a7e425d6ba6c3203216ee7f3ce453e
Sushil <sushilcg181@gmail.com>
2023-04-28 18:44:39 +0530

    added all the graphql queries

---
+++ src/graphqlQueries/GetProfile.js
@@ -0,0 +1,34 @@
+const GetProfile = `
+query getProfiles {
+    getProfiles(accountId: "123456", last_upd_time: "2023-03-21") {
+      status
+      data {
+        playerId
+      accountId
+      profileId
+      academyName
+      firstName
+      lastName
+      mobileNumber
+      emailId
+      dob
+      height
+      weight
+      gender
+      updateMeasurements
+      updateInterval
+      buildingName
+      street
+      locality
+      city
+      state
+      country
+      pinCode
+      location
+      areasOfIntrest
+      }
+      message
+    }
+  }
+`
+export default GetProfile;
---
+++ src/graphqlQueries/RegisterProfile.js
@@ -0,0 +1,33 @@
+const RegisterProfile = `
+mutation registerProfile {
+    registerProfile(registerProfileInput: {
+        playerId: "1238733",
+        accountId: "123456",
+        profileId: 45681,
+        academyName: "Embe Automations",
+        firstName: "John",
+        lastName: "Doe",
+        mobileNumber: "1234567890",
+        emailId: "johndoe@example.com",
+        dob: "2000-01-01",
+        height: "180",
+        weight: "75",
+        gender: "male",
+        updateMeasurements: "true",
+        updateInterval: "monthly",
+        buildingName: "Embe Complex",
+        street: "Red Bull Street",
+        locality: "Harley Quinn",
+        city: "Bangalore",
+        state: "Karnataka",
+        country: "India",
+        pinCode: "123456",
+        location: "Bangalore",
+        areasOfIntrest: ["Martial Arts", "Football"]
+      }) {
+      message
+      status
+      playerId
+    }
+  }
+`
