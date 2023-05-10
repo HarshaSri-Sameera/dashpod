7e4939b95241daa2d8cf23d81acbf71575d4d7c1
Sushil <sushilcg181@gmail.com>
2023-05-10 14:01:59 +0530

    added date picker

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -1,12 +1,12 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import CustomDatePicker from "./DatePicker";
 
-export default function ModalSection() {
+export default function ModalSection({ handleSubmit, handleFormChange }) {
   const [open, setOpen] = useState(true);
 
   const cancelButtonRef = useRef(null);
-
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -39,39 +39,184 @@ export default function ModalSection() {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationTriangleIcon
-                        className="h-6 w-6 text-red-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        Deactivate account
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
-                        </p>
+                <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                      <h2 className="text-base font-semibold leading-7 text-gray-900">
+                        Personal Information
+                      </h2>
+
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-3">
+                          <label
+                            htmlFor="first-name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            First name
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              name="first-name"
+                              id="first-name"
+                              autoComplete="given-name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                          <label
+                            htmlFor="last-name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Last name
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              name="last-name"
+                              id="last-name"
+                              autoComplete="family-name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
                       </div>
-                    </div>
+                      <h2 className="text-base font-semibold leading-7 text-gray-900">
+                        Gender
+                      </h2>
+                      <div className="flex items-center justify-between">
+                        {/*
+                            Gender Option
+                        */}
+                        <div className="flex items-center gap-x-3">
+                          <input
+                            id="gender-male"
+                            name="gender"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          />
+                          <label
+                            htmlFor="gender-male"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Male
+                          </label>
+                        </div>
+                        <div className="flex items-center gap-x-3">
+                          <input
+                            id="gender-female"
+                            name="gender"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          />
+                          <label
+                            htmlFor="gender-female"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Female
+                          </label>
+                        </div>
+                        <div className="flex items-center gap-x-3">
+                          <input
+                            id="gender-other"
+                            name="gender"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          />
+                          <label
+                            htmlFor="gender-other"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Others
+                          </label>
+                        </div>
+                      </div>
+                      <h2 className="text-base font-semibold leading-7 text-gray-900">
+                        DOB
+                      </h2>
+                      <div className="text-black">
+                        <CustomDatePicker />
+                      </div>
+                      <h2 className="text-base font-semibold leading-7 text-gray-900">
+                        Measurement
+                      </h2>
+                      {/* Measurement  */}
+                      <div className="flex justify-between align-between ">
+                        <div>
+                          <label
+                            htmlFor="price"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Weight
+                          </label>
+                          <div className="relative mt-2 rounded-md shadow-sm">
+                            <input
+                              type="number"
+                              name="weight"
+                              id="price"
+                              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              placeholder="0.00"
+                            />
+                            <div className="absolute inset-y-0 right-0 flex items-center">
+                              <label htmlFor="currency" className="sr-only">
+                                Measurement
+                              </label>
+                              <select
+                                id="currency"
+                                name="currency"
+                                className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                              >
+                                <option>KG</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="height"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Height
+                          </label>
+                          <div className="relative mt-2 rounded-md shadow-sm">
+                            <input
+                              type="number"
+                              name="weight"
+                              id="weight"
+                              className="block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              placeholder="0.00"
+                            />
+                            <div className="absolute inset-y-0 right-0 flex items-center">
+                              <label htmlFor="height" className="sr-only">
+                                Height
+                              </label>
+                              <select
+                                id="height"
+                                name="height"
+                                className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                              >
+                                <option>Inch</option>
+                                <option>Cm</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <button
+                          type="submit"
+                          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </form>
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                  >
-                    Deactivate
-                  </button>
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
